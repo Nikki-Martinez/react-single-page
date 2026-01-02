163123de74fbb0ca6732d97dcaa539bfc3fa3527
Nikki-Martinez <lmartinezj5@miumg.edu.gt>
2026-01-02 15:24:57 -0600

    se realizó formato principal del single-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Sidebar } from './components/Sidebar';
 import { Main } from './components/Main';
 import { Footer } from './components/Footer';
 import './index.css';
+import './App.css';
+
 
 export default function App() {
   return (
